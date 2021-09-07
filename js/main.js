a78551d49b291f569419203845b0c57162140247
umardeveloper <umar.abduganiyev@yandex.ru>
2021-09-07 21:09:37 +0500

    uy ishi js 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+alert("Assalomu aleykom 🖐 iltimos sorovnomani toldiring");
+alert(
+  "so'rovnomada ozingiz haqingizda malumotni toldirishingiz kerak boladi ☺️"
+);
+var firstName = prompt("Ismingiz nma?");
+console.log("ismingiz nma: " + firstName);
+var lastName = prompt("Familiyangizni kiriting");
+console.log("Familiyangizni kiriting" + lastName);
+var age = prompt("Yoshingiz nechida", 15);
+console.log("Yoshingiz nechida" + age);
+var whereAreYouFrom = prompt("Qayerliksiz");
+console.log("Qayerliksiz" + whereAreYouFrom);
+alert("Xurmatli dasturchi WWDCga xush kelibsiz welcome to WWDC");
